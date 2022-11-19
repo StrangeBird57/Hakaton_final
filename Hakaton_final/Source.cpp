b2c44027302aefcd29d16f4547d38d1841ef622f
Vladislav Zhukov <102047649+StrangeBird57@users.noreply.github.com>
2022-11-19 15:35:13 +0300

    Add class "Work"

--- Hakaton_final/Source.cpp
+++ Hakaton_final/Source.cpp
@@ -96,6 +96,30 @@ Point Markers::get_marker_4() {
     return marker_4;
 }
 
+class BotOperation {
+private:
+    string way;
+    ld time;
+public:
+    BotOperation();
+    BotOperation(string new_way, ld new_time = 0);
+    void print_data();
+};
+
+BotOperation::BotOperation() {
+    way = "stop";
+    time = 0;
+}
+
+BotOperation::BotOperation(string new_way, ld new_time = 0) {
+    way = new_way;
+    time = new_time;
+}
+
+void BotOperation::print_data() {
+    cout << way << " " << time << "\n";
+}
+
 class Workspace {
 private:
     Markers markers_screen, markers_floor;
@@ -108,6 +132,8 @@ public:
     void calculate_coefficiet();
     void calculate_robot_floor();
     void calculate_targets_floor();
+    Point get_robot();
+    vector <Point> get_targets();
 };
 
 Workspace::Workspace(){
@@ -157,6 +183,52 @@ void Workspace::calculate_targets_floor() {
         ld target_floor_y = robot_screen.get_y() * (y_coefficient_1 + y_coefficient_2) * dist_coefficient_y;
         Point target_floor(target_floor_x, target_floor_y);
         targets_floor[i] = target_floor;
+    }
+}
+
+Point Workspace::get_robot() {
+    return robot_floor;
+}
+
+vector <Point> Workspace::get_targets() {
+    return targets_floor;
+}
+
+class Work {
+private:
+    Point robot_start, robot_test;
+    vector <Point> targets;
+public:
+    Work();
+    void process();
+};
+
+Work::Work() {
+    Workspace start;
+    BotOperation test_go("forward", 1);
+    BotOperation test_back("back", 1);
+    test_go.print_data();
+    Workspace test;
+    test_back.print_data();
+    robot_start = start.get_robot();
+    robot_test = test.get_robot();
+    targets = start.get_targets();
+}
+
+void Work::process() {
+    //Line first(robot_start, robot_test), second(robot_start, targets[0]);
+    //ld angle = get_angle(first, second);
+    //ld get_dist(robot_start, targets[0])
+    //ld time_rotate = calculate_time_by_angle(angle);
+    //ld time_move = calculate_time_by_dist(dist);
+    //BotOperation rotate("left", time_rotate), move("forward", time_move), back("back", time_move);
+    for (int i = 1; i < targets.size(); ++i) {
+        //Line first(robot_start, targets[i - 1]), second(robot_start, targets[i]);
+        //ld angle = get_angle(first, second);
+        //ld get_dist(robot_start, targets[i])
+        //ld time_rotate = calculate_time_by_angle(angle);
+        //ld time_move = calculate_time_by_dist(dist);
+        //BotOperation rotate("left", time_rotate), move("forward", time_move), back("back", time_move);
     }
 }
 
